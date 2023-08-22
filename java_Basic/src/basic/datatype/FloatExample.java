4d0245b8f29bbb54e6d32878cd215afa996d39f6
minji <gkstnals2626@naver.com>
2023-08-22 17:15:01 +0900

    20230822 자바 기본 문법 (표준 출력 방식까지)

---
+++ java_Basic/src/basic/datatype/FloatExample.java
@@ -0,0 +1,21 @@
+package basic.datatype;
+
+public class FloatExample {
+
+	public static void main(String[] args) {
+
+		//실수 리터럴의 기본 데이터 타입은 double 입니다.
+		float f1 = 7.12345F;
+		double d1 = 7.12345;
+		
+		/////////////////////////////////////////////
+		
+		float f2 = 1.234567891234F;
+		double d2 = 1.234567891234;
+		
+		System.out.println(f2); // Ctrl+Alt+방향키↓ 현재 줄이 복사가 되어 밑에 붙여넣기가 됨
+		System.out.println(f2);
+		
+	}
+
+}
